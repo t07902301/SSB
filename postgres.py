427d1dda18361d7dc2509d8d097d7976d87493a7
Yiwei(Amber) Liu <e-well@outlook.com>
2021-12-28 16:38:04 -0500

    baby online learning

--- postgres.py
+++ postgres.py
@@ -1,11 +1,22 @@
 import psycopg2
-try:
-    conn = psycopg2.connect(
-        host="localhost",
-        database="ssb",
-        user="postgres",
-    password="")
-except Exception as error:
-    print(error)
+def connect():
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="ssb",
+            user="postgres",
+        password="")
+        return conn
+    except Exception as error:
+        print(error)
+def delopy(replicated_table,partition):
+    replicated_tbls=[]
+    partition_tbls=[]
+    for tbl,replicated in replicated_table.items():
+        if replicated:
+            replicated_tbls.append(tbl)
+    for tbl,partitioned in partition.items():
+        if partitioned:
+            partition_tbls.append(tbl)
 # conn = create_postgres_conn(config, dataset)
 # content = explain_sql(sql, conn, honor_join_order, format)
